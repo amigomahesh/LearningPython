35cc8afbae05b1172831583ee15a52cb105f1e2f
Mahesh <kalledamahesh0@gmail.com>
2024-05-17 15:46:21 +0530

    Simple testing added

---
+++ simple_test.py
@@ -0,0 +1,48 @@
+# conditional
+# age limit of driving license
+age = int(input("eligible for age: "))
+if age >= 18 and age < 30:
+    print("eligible for license")
+elif age >= 30 and age <=57:
+    print("eligible for both license")
+elif age >57:
+    print("not eligible")
+else:
+    print("not eligible")
+
+# Integer = 100
+# print(Integer)
+
+# all arthematic formulas are applied in taple:
+# age = (100, 5000, 7000, 50)
+# min_age = min(age)
+# print(min_age)
+
+# list = [10, 20, 30, 50, 80, 100, 150, 180, 200]
+# list_max = max(list)
+# additional_values = (f"[500, 700, 800, 1000, 10]")
+# list.remove[0:5]
+# print(list)
+
+
+
+# age = (100, 5000)
+# max_age = max(age)
+# print(max_age)
+
+
+# float = 100.500
+# print(float)
+
+# float = 100.50000
+# print('%.3f' % float)
+
+# char = "with three decimal points. \n Adjust the precision specifier"
+# print(char)
+
+# complex = 10 + k3
+# print(complex)
+
+
+
+
